846de11cbe20d3e966fa04efac13804ee60d4a03
Jovan Maksimoski <jovanmaksimoskii@gmail.com>
2024-10-19 12:22:05 +0200

    added some design and routes for managing data and crud operations

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/manage', function () {
+    return view('manage');
+})->name('manage');
 require __DIR__.'/auth.php';
