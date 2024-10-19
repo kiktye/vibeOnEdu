7699312a01ddb9d1e36c065e4406513a723be14c
kiktye <kikostojanov4466@yahoo.com>
2024-10-19 16:44:18 +0200

    web

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,26 @@ Route::middleware('auth')->group(function () {
         Route::put('{badge}', [BadgeController::class, 'update'])->name('update');
         Route::delete('{badge}', [BadgeController::class, 'destroy'])->name('destroy');
     });
+
+    Route::prefix('modules')->name('modules.')->group(function () {
+        Route::get('/', [\App\Http\Controllers\ModuleController::class, 'index'])->name('index');
+        Route::get('{module}', [\App\Http\Controllers\ModuleController::class, 'show'])->name('show');
+
+        Route::post('/', [\App\Http\Controllers\ModuleController::class, 'store'])->name('store');
+
+        Route::delete('{module}', [\App\Http\Controllers\ModuleController::class, 'destroy'])->name('destroy');
+        Route::put('{modules}', [\App\Http\Controllers\ModuleController::class, 'update'])->name('update');
+    });
+
+    Route::controller(LectureController::class)->group(function () {
+        Route::get('/lectures', 'index')->name('lectures.index');
+        Route::get('/lectures/create', 'create')->name('lectures.create');
+        Route::post('/lectures/create', 'store')->name('lectures.store');
+        Route::get('/lectures/{lecture}', 'show')->name('lectures.show');
+        Route::get('/lectures/{lecture}/edit', 'edit')->name('lectures.edit');
+        Route::put('/lectures/{lecture}', 'update')->name('lectures.update');
+        Route::delete('/lectures/{lecture}', 'destroy')->name('lectures.destroy');
+    });
 });
 
 
@@ -57,29 +77,6 @@ Route::get('/manage', function () {
 })->name('manage');
 
 
-<<<<<<< HEAD
+
+
 require __DIR__ . '/auth.php';
-=======
-Route::prefix('modules')->name('modules.')->group(function () {
-    Route::get('/', [\App\Http\Controllers\ModuleController::class, 'index'])->name('index');
-    Route::get('{module}', [\App\Http\Controllers\ModuleController::class, 'show'])->name('show');
-
-    Route::post('/', [\App\Http\Controllers\ModuleController::class, 'store'])->name('store');
-
-    Route::delete('{module}', [\App\Http\Controllers\ModuleController::class, 'destroy'])->name('destroy');
-    Route::put('{modules}', [\App\Http\Controllers\ModuleController::class, 'update'])->name('update');
-
-});
-
-require __DIR__.'/auth.php';
->>>>>>> 53ea72a9b6229834c95c66124f165bc13512e79d
-
-Route::controller(LectureController::class)->group(function () {
-    Route::get('/lectures', 'index')->name('lectures.index');
-    Route::get('/lectures/create', 'create')->name('lectures.create');
-    Route::post('/lectures/create', 'store')->name('lectures.store');
-    Route::get('/lectures/{lecture}', 'show')->name('lectures.show');
-    Route::get('/lectures/{lecture}/edit', 'edit')->name('lectures.edit');
-    Route::put('/lectures/{lecture}', 'update')->name('lectures.update');
-    Route::delete('/lectures/{lecture}', 'destroy')->name('lectures.destroy');
-});
