a119444b789df25b90b587a399d244f2ad9241ec
kiktye <kikostojanov4466@yahoo.com>
2024-10-19 16:46:43 +0200

    head conflicts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,57 +8,50 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-<<<<<<< HEAD
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-=======
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-
-        <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
-
-    </head>
-    <body class="font-sans antialiased">
-{{--        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">--}}
-{{--            @include('layouts.navigation')--}}
->>>>>>> 53ea72a9b6229834c95c66124f165bc13512e79d
-
     <!-- Scripts -->
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+
 </head>
 
 <body class="font-sans antialiased">
     {{--        <div class="min-h-screen bg-gray-100 dark:bg-gray-900"> --}}
     {{--            @include('layouts.navigation') --}}
 
-    {{--            <!-- Page Heading --> --}}
-    {{--            @if (isset($header)) --}}
-    {{--                <header class="bg-white dark:bg-gray-800 shadow"> --}}
-    {{--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"> --}}
-    {{--                        {{ $header }} --}}
-    {{--                    </div> --}}
-    {{--                </header> --}}
-    {{--            @endif --}}
+    <!-- Scripts -->
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
 
-    {{--            <!-- Page Content --> --}}
-    {{--            <main> --}}
-    {{--                {{ $slot }} --}}
-    {{--            </main> --}}
-    {{--        </div> --}}
-</body>
+    <body class="font-sans antialiased">
+        {{--        <div class="min-h-screen bg-gray-100 dark:bg-gray-900"> --}}
+        {{--            @include('layouts.navigation') --}}
 
-<<<<<<< HEAD
-=======
-{{--            <!-- Page Content -->--}}
-{{--            <main>--}}
-{{--                {{ $slot }}--}}
-{{--            </main>--}}
-{{--        </div>--}}
-<script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+        {{--            <!-- Page Heading --> --}}
+        {{--            @if (isset($header)) --}}
+        {{--                <header class="bg-white dark:bg-gray-800 shadow"> --}}
+        {{--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8"> --}}
+        {{--                        {{ $header }} --}}
+        {{--                    </div> --}}
+        {{--                </header> --}}
+        {{--            @endif --}}
 
+        {{--            <!-- Page Content --> --}}
+        {{--            <main> --}}
+        {{--                {{ $slot }} --}}
+        {{--            </main> --}}
+        {{--        </div> --}}
     </body>
+
+    <<<<<<< HEAD======={{--            <!-- Page Content --> --}} {{--            <main> --}} {{--                {{ $slot }} --}} {{--            </main> --}}
+        {{--        </div> --}} <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+
+</body>
 >>>>>>> 53ea72a9b6229834c95c66124f165bc13512e79d
+
 </html>
