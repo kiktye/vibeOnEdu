b6cbb626f56a7c0e24ad921121c6f7257e89d253
kiktye <kikostojanov4466@yahoo.com>
2024-10-19 10:37:10 +0200

    fix db migrations

--- database/migrations/2024_10_19_080942_create_lectures_table.php
+++ database/migrations/2024_10_19_080942_create_lectures_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->string('audio_path')->nullable();
-            $table->int('duration')->nullable();
+            $table->integer('duration')->nullable();
             $table->timestamps();
         });
     }
